06e655c9d0eccdd7981729eff3590e3e9c64c086
bhanford9 <bmhanford@sep.com>
2021-11-10 18:13:52 -0500

    xaml cleanup and start to setup tab page

---
+++ DataAnalyzer/Views/ConfigurationSetupTabView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataAnalyzer.Views
+{
+  /// <summary>
+  /// Interaction logic for ConfigurationSetupTabView.xaml
+  /// </summary>
+  public partial class ConfigurationSetupTabView : UserControl
+  {
+    public ConfigurationSetupTabView()
+    {
+      InitializeComponent();
+    }
+  }
+}
